94ce7464b1c8eb02e7fec97d8a3f10f7f61df072
HopeMabuza <londiwemabuza94@gmail.com>
2026-08-19 23:05:20 +0200

    added state variables and initializer

--- packages/hardhat/contracts/staking.sol
+++ packages/hardhat/contracts/staking.sol
@@ -12,18 +12,25 @@ contract Staking is Initializable, UUPSUpgradeable, OwnableUpgradeable, Reentran
     //state variables
     IERC20 public stakingToken;
 
-    struct Stake{
+    struct stakeInfo {
         uint256 stakedAmount;
         uint256 startTime;
         uint256 lastClaimed;
         bool active;
     }
 
-    mapping (address => Stake[]) public userStakes;
+    struct Tier{
+        uint256 lockDuration;
+        uint256 dailyRate;
+    }
+    Tier public tier;
+    
+    mapping (address => stakeInfo[]) public userStakes;
 
-    uint256 public period;
+
+    uint256 public period;//in seconds
     uint256 public taxRate;
-    uint256 public rewardsRate;
+    uint256 public denominator;
     address public devWallet;
     
     uint256 public totalTaxed;
@@ -31,10 +38,9 @@ contract Staking is Initializable, UUPSUpgradeable, OwnableUpgradeable, Reentran
     uint256 public rewardsPool;
 
     //events
-    event Deposited(address indexed user, uint256 indexed stakeIndex, uint256 amount);
+    event Stake(address indexed user, uint256 indexed stakeIndex, uint256 amount);
     event ClaimedRewards(address indexed user, uint256 indexed stakeIndex, uint256 amount);
-    event Withdrawal(address indexed user, uint256 indexed stakeIndex, uint256 amount);
-    event WithdrawStuckTokens(address owner, uint256 amount);
+    event Unstake(address indexed user, uint256 indexed stakeIndex, uint256 amount);
     
 
     //disbale initializer
@@ -44,10 +50,39 @@ contract Staking is Initializable, UUPSUpgradeable, OwnableUpgradeable, Reentran
     }
 
     //initializer
+    function initialize(address _stakingToken, address _devWallet) public initializer{
+        stakingToken = IERC20(_stakingToken);
+        devWallet = _devWallet;
+        period = 60;
+        taxRate = 200;
+        denominator = 10000;
+        tier = Tier(600, 3000);
+
+    }
+
 
     //core functions
+    function stake(uint256 _amount) public {}
 
-    //getters and setters
+    function claimRewards(uint256 _stakeIndex) public {}
+
+    function unstake(uint256 _stakeIndex) public {
+        //apply transaction fee
+    }
+
+    //helper functions
+    function calculateRewards(address _user, uint256 _stakeIndex) public view returns(uint256){}
+
+    //admit functions
+    function resetTier(uint256 _lockDuration, uint256 _percentageRate) external onlyOwner{
+        tier = Tier(_lockDuration, _percentageRate);
+    }
+
+    function fundRewardPool(uint256 _amount) external onlyOwner{}
+
+    function resetPeriod(uint256 _period) external onlyOwner {
+        period = _period;
+    }
 
     //authorize upgradeable
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
